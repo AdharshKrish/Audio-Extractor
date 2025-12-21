37db7a6756835b193eda193a6e670fcf938ce63a
Adharsh <adharshkrish@outlook.com>
2025-12-21 10:27:57 +0530

    Add script to download and extract latest FFmpeg

---
+++ to-download-latest-ffmpeg.sh
@@ -0,0 +1,8 @@
+# 1. Download the latest build
+wget https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz
+
+# 2. Extract it
+tar -xf ffmpeg-release-amd64-static.tar.xz
+
+# 3. Rename the folder to something simple (like 'ffmpeg-new')
+mv ffmpeg-*-amd64-static ffmpeg-new
